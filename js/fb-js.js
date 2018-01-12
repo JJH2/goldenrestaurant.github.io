a053b2c1b470e127957ed2c52a27a6954484bac0
Jang Jaehoon <idlinlee12@gmail.com>
2018-01-12 22:11:04 +0900

    finish

--- js/fb-js.js
+++ js/fb-js.js
@@ -1,18 +1,18 @@
-const provider = new firebase.auth.GoogleAuthProvider();
-const auth = firebase.auth();
-const database = firebase.database();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const auth = firebase.auth();
+// const database = firebase.database();
 
 
 
-async function authSign() {
-    const result = await auth.signInWithPopup(provider);
-    let token = result.credential.accessToken;
-    // The signed-in user info.
-    let user = result.user;
-}
+// async function authSign() {
+//     const result = await auth.signInWithPopup(provider);
+//     let token = result.credential.accessToken;
+//     // The signed-in user info.
+//     let user = result.user;
+// }
 
-const loginBtn = document.querySelector('.login');
-loginBtn.addEventListener('click', e => {
-    authSign();
-})
+// const loginBtn = document.querySelector('.login');
+// loginBtn.addEventListener('click', e => {
+//     authSign();
+// })
 
